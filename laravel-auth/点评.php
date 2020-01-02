8c8b08ab7cb011ce28293aa94ba20783fdb70cac
ccjian <377148474@qq.com>
2020-01-02 11:29:47 +0800

    1

---
+++ laravel-auth/点评.php
@@ -0,0 +1,23 @@
+<?php
+/*
+https://learnku.com/articles/38735
+auth
+
+思想
+    配置化
+        resolve解析配置
+        变量方法依赖配置
+        switch分类依赖配置生成provider
+        注入对象依赖配置
+        provider注入调用固有依赖约束方法
+    Facade解耦
+
+
+
+
+
+
+
+
+
+
