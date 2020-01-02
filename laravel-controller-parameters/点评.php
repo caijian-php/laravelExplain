67c969aa516f0e3921a7f8f1e8280fb395358ff4
ccjian <377148474@qq.com>
2020-01-02 15:40:32 +0800

    1

--- laravel-controller-parameters/点评.php
+++ laravel-controller-parameters/点评.php
@@ -2,19 +2,52 @@
 /*
 https://learnku.com/articles/38911
 
-routeDependencyResolverTrait resolveMethodDependencies
-$reflector->getParameters()
-$router->parametersWithoutNulls()
+事件触发
+runRoute
+    return $this->prepareResponse($request,
+        $this->runRouteWithinStack($route, $request)
+    );
+        return (new Pipeline($this->container))
+        ->send($request)
+        ->through($middleware)
+        ->then(function ($request) use ($route) {
+            return $this->prepareResponse(
+                $request, $route->run()
+            );
+        });
+            $route->run()
+                $this->runController();
+                    return $this->controllerDispatcher()->dispatch(
+                        $this, $this->getController(), $this->getControllerMethod()
+                    );
+                        $parameters = $this->resolveClassMethodDependencies(
+                            $route->parametersWithoutNulls(), $controller, $method
+                        );
+                            resolveClassMethodDependencies();
+                                resolveMethodDependencies();
+                                    $instance = $this->transformDependency(
+                                        $parameter, $parameters
+                                    );
+                                        this->container->make($class->name);
+                                            return $this->resolve
+                                                $this->fireResolvingCallbacks($abstract, $object);
+                                                    $this->fireAfterResolvingCallbacks($abstract, $object);
 
+触发获取
+Illuminate\Foundation\Providers function boot()
+    $this->app->afterResolving(\Illuminate\Contracts\Validation\ValidatesWhenResolved::class, function ($resolved) {
+        resolved->validateResolved();
+    });
+        $resolved->validateResolved();
+            $instance = $this->getValidatorInstance();
+                 $validator = $this->createDefaultValidator
+                    return $factory->make()
+                        \Illuminate\Validation\Factory->make()
+                            \Illuminate\Validation\Factory->resolve()
+                                return new \Illuminate\Validation\Validator(...)
 
-
-
-
-
-
-
-
-
-
+获取调用
+$this->fails();
+    $this->passes()
 
 
