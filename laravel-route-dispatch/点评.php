4a155e7a01da1693fcd84b81b481320065e41fee
ccjian <377148474@qq.com>
2020-01-02 10:33:53 +0800

    1

--- laravel-route-dispatch/点评.php
+++ laravel-route-dispatch/点评.php
@@ -4,4 +4,11 @@ https://learnku.com/articles/38633
 runRoute
 
 路由分发
-
+    中间件是否启用
+    收集中间件
+        当前Controller+Action决定middleware
+    路由分发器ControllerDispatcher
+        通过getMiddleware返回collect收集中相应的middleware
+    run->runController->dispatch by controller,method,array_value($parameters)
+        parameter from resolveClassMethodDependencies
+        if method_exists(callAction) run it
