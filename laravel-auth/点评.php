898cb448e9aee67f116abe8160cedd69f9624200
ccjian <377148474@qq.com>
2020-01-02 11:40:00 +0800

    1

--- laravel-auth/点评.php
+++ laravel-auth/点评.php
@@ -5,11 +5,11 @@ auth
 
 思想
     配置化
-        resolve解析配置
-        变量方法依赖配置
-        switch分类依赖配置生成provider
-        注入对象依赖配置
-        provider注入调用固有依赖约束方法
+        resolve配置解析guard
+        变量方法create.ucFirst().Driver依赖配置生成guards.driver
+        create...Provider switch分类provider.driver依赖配置生成provider注入guards.driver
+        注入对象provider依赖配置
+    使用时provider注入调用固有依赖约束方法
     Facade解耦
 
 
